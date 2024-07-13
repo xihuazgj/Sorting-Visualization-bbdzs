7f6f1a5043cc2763fc8f0fe8e05df6e70ce374c8
guojunzhang <1965572081@qq.com>
2024-07-13 14:50:08 +0800

    Initial commit

---
+++ build-Sorting_Visulization_cqz-Desktop_Qt_5_15_2_MinGW_32_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,231 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../Sorting_Visulization_cqz/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[36];
+    char stringdata0[527];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 25), // "on_btn_dataImport_clicked"
+QT_MOC_LITERAL(2, 37, 0), // ""
+QT_MOC_LITERAL(3, 38, 27), // "on_btn_dataGenerate_clicked"
+QT_MOC_LITERAL(4, 66, 27), // "on_btn_sortingStart_clicked"
+QT_MOC_LITERAL(5, 94, 27), // "on_btn_sortingPause_clicked"
+QT_MOC_LITERAL(6, 122, 30), // "on_btn_sortingContinue_clicked"
+QT_MOC_LITERAL(7, 153, 22), // "on_btn_oneStep_clicked"
+QT_MOC_LITERAL(8, 176, 20), // "menuAct_actRadixSort"
+QT_MOC_LITERAL(9, 197, 20), // "menuAct_actQuickSort"
+QT_MOC_LITERAL(10, 218, 20), // "menuAct_actMergeSort"
+QT_MOC_LITERAL(11, 239, 19), // "menuAct_actHeapSort"
+QT_MOC_LITERAL(12, 259, 20), // "menuAct_actShellSort"
+QT_MOC_LITERAL(13, 280, 21), // "menuAct_actInsertSort"
+QT_MOC_LITERAL(14, 302, 21), // "menuAct_actBubbleSort"
+QT_MOC_LITERAL(15, 324, 21), // "menuAct_actSelectSort"
+QT_MOC_LITERAL(16, 346, 8), // "setSpeed"
+QT_MOC_LITERAL(17, 355, 5), // "speed"
+QT_MOC_LITERAL(18, 361, 16), // "sortingAlgorithm"
+QT_MOC_LITERAL(19, 378, 5), // "index"
+QT_MOC_LITERAL(20, 384, 13), // "updateColumns"
+QT_MOC_LITERAL(21, 398, 1), // "a"
+QT_MOC_LITERAL(22, 400, 1), // "b"
+QT_MOC_LITERAL(23, 402, 4), // "int*"
+QT_MOC_LITERAL(24, 407, 10), // "newHeights"
+QT_MOC_LITERAL(25, 418, 15), // "updateOneColumn"
+QT_MOC_LITERAL(26, 434, 1), // "c"
+QT_MOC_LITERAL(27, 436, 9), // "resetPrev"
+QT_MOC_LITERAL(28, 446, 16), // "highLightColumns"
+QT_MOC_LITERAL(29, 463, 13), // "highLightBase"
+QT_MOC_LITERAL(30, 477, 4), // "base"
+QT_MOC_LITERAL(31, 482, 10), // "showFinish"
+QT_MOC_LITERAL(32, 493, 17), // "updateSortingTime"
+QT_MOC_LITERAL(33, 511, 6), // "sbShow"
+QT_MOC_LITERAL(34, 518, 3), // "num"
+QT_MOC_LITERAL(35, 522, 4) // "text"
+
+    },
+    "MainWindow\0on_btn_dataImport_clicked\0"
+    "\0on_btn_dataGenerate_clicked\0"
+    "on_btn_sortingStart_clicked\0"
+    "on_btn_sortingPause_clicked\0"
+    "on_btn_sortingContinue_clicked\0"
+    "on_btn_oneStep_clicked\0menuAct_actRadixSort\0"
+    "menuAct_actQuickSort\0menuAct_actMergeSort\0"
+    "menuAct_actHeapSort\0menuAct_actShellSort\0"
+    "menuAct_actInsertSort\0menuAct_actBubbleSort\0"
+    "menuAct_actSelectSort\0setSpeed\0speed\0"
+    "sortingAlgorithm\0index\0updateColumns\0"
+    "a\0b\0int*\0newHeights\0updateOneColumn\0"
+    "c\0resetPrev\0highLightColumns\0highLightBase\0"
+    "base\0showFinish\0updateSortingTime\0"
+    "sbShow\0num\0text"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      23,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  129,    2, 0x08 /* Private */,
+       3,    0,  130,    2, 0x08 /* Private */,
+       4,    0,  131,    2, 0x08 /* Private */,
+       5,    0,  132,    2, 0x08 /* Private */,
+       6,    0,  133,    2, 0x08 /* Private */,
+       7,    0,  134,    2, 0x08 /* Private */,
+       8,    0,  135,    2, 0x08 /* Private */,
+       9,    0,  136,    2, 0x08 /* Private */,
+      10,    0,  137,    2, 0x08 /* Private */,
+      11,    0,  138,    2, 0x08 /* Private */,
+      12,    0,  139,    2, 0x08 /* Private */,
+      13,    0,  140,    2, 0x08 /* Private */,
+      14,    0,  141,    2, 0x08 /* Private */,
+      15,    0,  142,    2, 0x08 /* Private */,
+      16,    1,  143,    2, 0x08 /* Private */,
+      18,    1,  146,    2, 0x08 /* Private */,
+      20,    3,  149,    2, 0x08 /* Private */,
+      25,    3,  156,    2, 0x08 /* Private */,
+      28,    2,  163,    2, 0x08 /* Private */,
+      29,    1,  168,    2, 0x08 /* Private */,
+      31,    0,  171,    2, 0x08 /* Private */,
+      32,    0,  172,    2, 0x08 /* Private */,
+      33,    2,  173,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   17,
+    QMetaType::Void, QMetaType::Int,   19,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int, 0x80000000 | 23,   21,   22,   24,
+    QMetaType::Void, QMetaType::Int, QMetaType::Bool, 0x80000000 | 23,   26,   27,   24,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,   21,   22,
+    QMetaType::Void, QMetaType::Int,   30,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::QString,   34,   35,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_btn_dataImport_clicked(); break;
+        case 1: _t->on_btn_dataGenerate_clicked(); break;
+        case 2: _t->on_btn_sortingStart_clicked(); break;
+        case 3: _t->on_btn_sortingPause_clicked(); break;
+        case 4: _t->on_btn_sortingContinue_clicked(); break;
+        case 5: _t->on_btn_oneStep_clicked(); break;
+        case 6: _t->menuAct_actRadixSort(); break;
+        case 7: _t->menuAct_actQuickSort(); break;
+        case 8: _t->menuAct_actMergeSort(); break;
+        case 9: _t->menuAct_actHeapSort(); break;
+        case 10: _t->menuAct_actShellSort(); break;
+        case 11: _t->menuAct_actInsertSort(); break;
+        case 12: _t->menuAct_actBubbleSort(); break;
+        case 13: _t->menuAct_actSelectSort(); break;
+        case 14: _t->setSpeed((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: _t->sortingAlgorithm((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 16: _t->updateColumns((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int*(*)>(_a[3]))); break;
+        case 17: _t->updateOneColumn((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2])),(*reinterpret_cast< int*(*)>(_a[3]))); break;
+        case 18: _t->highLightColumns((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 19: _t->highLightBase((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 20: _t->showFinish(); break;
+        case 21: _t->updateSortingTime(); break;
+        case 22: _t->sbShow((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 23)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 23;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 23)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 23;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
